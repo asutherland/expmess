1811ad95144f890668fe289eea2b511174e9d0c3
Andrew Sutherland <asutherland@asutherland.org>
2008-06-09 21:05:40 -0400

    initial import from generation by Ted Mielczarek's extensionwiz, plus eclipse .project.

---
+++ defaults/preferences/expmess.js
@@ -0,0 +1,5 @@
+pref("extensions.expmess.boolpref", false);
+pref("extensions.expmess.intpref", 0);
+pref("extensions.expmess.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.expmess@mozillamessaging.com.description", "chrome://expmess/locale/expmess.properties");
